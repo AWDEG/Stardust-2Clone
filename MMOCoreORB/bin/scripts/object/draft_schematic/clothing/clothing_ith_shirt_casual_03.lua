901a3cb9e8af46bf75eddc253102f3b6a5c9d457
AWDEG <54282459+AWDEG@users.noreply.github.com>
2024-06-28 01:13:23 -0500

    Update clothing_ith_shirt_casual_03.lua
    Clothing Normalization

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_ith_shirt_casual_03.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_ith_shirt_casual_03.lua
@@ -63,11 +63,11 @@ object_draft_schematic_clothing_clothing_ith_shirt_casual_03 = object_draft_sche
    customizationDefaults = {},
 
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"body", "binding_and_hardware"},
-   ingredientSlotType = {1, 0},
-   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast"},
-   resourceQuantities = {2, 30},
-   contribution = {100, 100},
+   ingredientTitleNames = {"body", "binding_and_hardware", "stitching"},
+   ingredientSlotType = {1, 0, 1},
+   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceQuantities = {1, 50, 1},
+   contribution = {100, 100, 100},
 
 
    targetTemplate = "object/tangible/wearables/ithorian/ith_shirt_s03.iff",
