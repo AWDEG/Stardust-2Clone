88e8e2625c86f8ddc81d20ca539a8ccd1377e79a
AWDEG <54282459+AWDEG@users.noreply.github.com>
2024-07-09 01:50:05 -0500

    Update clothing_hat_field_04.lua
    Clothing Normalization

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_hat_field_04.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_hat_field_04.lua
@@ -64,9 +64,9 @@ object_draft_schematic_clothing_clothing_hat_field_04 = object_draft_schematic_c
 
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"shell", "binding_and_weatherproofing", "liner"},
-   ingredientSlotType = {0, 0, 1},
-   resourceTypes = {"steel", "petrochem_inert", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
-   resourceQuantities = {25, 20, 1},
+   ingredientSlotType = {1, 0, 1},
+   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "petrochem_inert", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceQuantities = {1, 25, 1},
    contribution = {100, 100, 100},
 
 
