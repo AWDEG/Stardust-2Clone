ed1c4ecc6577254232b1a6136788ca1de7dc1730
AWDEG <54282459+AWDEG@users.noreply.github.com>
2024-07-12 10:44:00 -0500

    Update clothing_ith_shirt_field_05.lua
    Clothing Balance

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_ith_shirt_field_05.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_ith_shirt_field_05.lua
@@ -64,9 +64,9 @@ object_draft_schematic_clothing_clothing_ith_shirt_field_05 = object_draft_schem
 
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"body", "trim", "binding_and_hardware"},
-   ingredientSlotType = {1, 0, 0},
-   resourceTypes = {"object/tangible/component/clothing/shared_reinforced_fiber_panels.iff", "fiberplast", "metal"},
-   resourceQuantities = {1, 20, 10},
+   ingredientSlotType = {1, 1, 0},
+   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "metal"},
+   resourceQuantities = {1, 1, 50},
    contribution = {100, 100, 100},
 
 
