50488fe9a44bd8ef3614f8cfacae877d89559295
AWDEG <54282459+AWDEG@users.noreply.github.com>
2024-07-15 13:37:47 -0500

    Update clothing_shirt_casual_16.lua
    Clothing balance

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_shirt_casual_16.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_shirt_casual_16.lua
@@ -62,12 +62,12 @@ object_draft_schematic_clothing_clothing_shirt_casual_16 = object_draft_schemati
    customizationStringNames = {"/private/index_color_1"},
    customizationDefaults = {47},
 
-   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"body", "binding_and_hardware"},
-   ingredientSlotType = {1, 0},
-   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast"},
-   resourceQuantities = {1, 30},
-   contribution = {100, 100},
+   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   ingredientTitleNames = {"body", "binding_and_hardware", "stitching"},
+   ingredientSlotType = {1, 0, 1},
+   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceQuantities = {1, 50, 1},
+   contribution = {100, 100, 100},
 
 
    targetTemplate = "object/tangible/wearables/shirt/shirt_s16.iff",
