64e007a02a881f9a239acb2f5db0ea49af83da7d
AWDEG <54282459+AWDEG@users.noreply.github.com>
2024-07-18 16:54:54 -0500

    Update clothing_wke_shirt_s04.lua
    Clothing balance

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_wke_shirt_s04.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_wke_shirt_s04.lua
@@ -65,8 +65,8 @@ object_draft_schematic_clothing_clothing_wke_shirt_s04 = object_draft_schematic_
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"heavy_shell", "binding_and_weatherproofing", "hardware", "liner_half", "liner_half2"},
    ingredientSlotType = {0, 0, 1, 1, 1},
-   resourceTypes = {"fiberplast", "petrochem_inert", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
-   resourceQuantities = {15, 15, 1, 1, 1},
+   resourceTypes = {"hide", "petrochem_inert", "object/tangible/component/clothing/shared_metal_fasteners.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceQuantities = {25, 20, 1, 1, 1},
    contribution = {100, 100, 100, 100, 100},
 
 
