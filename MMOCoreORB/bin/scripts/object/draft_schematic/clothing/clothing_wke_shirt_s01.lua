c8c1279a083d5b1e7e137b5d70c8dd27781878a9
AWDEG <54282459+AWDEG@users.noreply.github.com>
2024-07-18 16:55:16 -0500

    Update clothing_wke_shirt_s01.lua
    Clothing balance

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_wke_shirt_s01.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_wke_shirt_s01.lua
@@ -62,12 +62,12 @@ object_draft_schematic_clothing_clothing_wke_shirt_s01 = object_draft_schematic_
    customizationStringNames = {"/private/index_color_1", "/private/index_color_2"},
    customizationDefaults = {8, 22},
 
-   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"heavy_shell", "binding_and_weatherproofing"},
-   ingredientSlotType = {0, 0},
-   resourceTypes = {"fiberplast", "petrochem_inert_polymer"},
-   resourceQuantities = {18, 2},
-   contribution = {100, 100},
+   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   ingredientTitleNames = {"heavy_shell", "binding_and_weatherproofing", "hardware", "liner_half", "liner_half2"},
+   ingredientSlotType = {0, 0, 1, 1, 1},
+   resourceTypes = {"hide", "petrochem_inert", "object/tangible/component/clothing/shared_metal_fasteners.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceQuantities = {25, 20, 1, 1, 1},
+   contribution = {100, 100, 100, 100, 100},
 
 
    targetTemplate = "object/tangible/wearables/wookiee/wke_shirt_s01.iff",
